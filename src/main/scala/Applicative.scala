b592cdb765fba9da2116ed36e6b1b6c4cb2a2cfd
lemastero <adres.mistrza@gmail.com>
2016-04-21 09:09:14 +0200

    Applicative functor trait

---
+++ src/main/scala/Applicative.scala
@@ -0,0 +1,31 @@
+/*
+ * Based on awesome video serie: Functional Structures in Scala
+ * by: Michael Pilquist
+ * part: FSiS Part 2 - Applicative type class
+ * https://www.youtube.com/watch?v=tD_EyIKqqCk
+ */
+
+import simulacrum._
+
+/**
+  * Applicative functor.
+  *
+  * map apply function in context (effect)
+  * pure put in context (effect)
+  *
+  * More stuff:
+  *  The Essence of the Iterator Pattern: https://www.cs.ox.ac.uk/jeremy.gibbons/publications/iterator.pdf
+  *  Applicative programming with effects: http://staff.city.ac.uk/~ross/papers/Applicative.pdf
+  *
+  * Effect might not be side effect
+  * Examples of effects:
+  *   Option - effect of having a value or not having a value
+  *   LIst - effect of having multiple values
+  */
+@typeclass trait Applicative[Effect[_]] extends Functor[Effect] {
+
+  /**
+    * Take value and put (lift) into effect
+    */
+  def pure[A](value: A): Effect[A]
+}
