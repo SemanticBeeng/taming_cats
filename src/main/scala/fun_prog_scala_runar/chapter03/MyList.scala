ec40987986bf5dbaa6d68608eca617548c60a60a
lemastero <adres.mistrza@gmail.com>
2016-04-27 09:42:20 +0200

    Chapter 3 from book Functional Programming in Scala

---
+++ src/main/scala/fun_prog_scala_runar/chapter03/MyList.scala
@@ -0,0 +1,46 @@
+package fun_prog_scala_runar.chapter03
+
+sealed trait MyList[+A] {
+
+  def tail: MyList[A]
+
+  def drop(n: Int): MyList[A] = this match {
+    case Nil => this
+    case Cons(h, t) => if(n<=0) this else t.drop(n-1)
+  }
+
+  def dropWhile(f: A => Boolean): MyList[A] = this match {
+    case Nil => this
+    case Cons(h,t) => if(f(h)) t.dropWhile(f) else this
+  }
+
+//  def setHead(a: A): MyList[A] = this match { TODO contravariant arg error
+//    case Nil => Cons(a, Nil)
+//    case Cons(h, t) => Cons(a, t)
+//  }
+}
+
+case object Nil extends MyList[Nothing] {
+
+  override def tail: MyList[Nothing] = this
+}
+
+case class Cons[+A](head: A, tail: MyList[A]) extends MyList[A]
+
+object MyList {
+
+  def sum(ints: MyList[Int]): Int = ints match {
+    case Nil => 0
+    case Cons(x, xs) => x + sum(xs)
+  }
+
+  def product(ds: MyList[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs) => x * product(xs)
+  }
+
+  def apply[A](as: A*): MyList[A] = // variadic function (Java.. varargs) in Scala it is Seq
+    if(as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail:_*))
+}
