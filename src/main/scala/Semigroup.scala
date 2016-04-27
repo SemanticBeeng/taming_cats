732dd018556f0b8676097ab91c467e26fb774841
lemastero <adres.mistrza@gmail.com>
2016-04-27 09:39:02 +0200

    Semigroup start of implementation

---
+++ src/main/scala/Semigroup.scala
@@ -0,0 +1,15 @@
+import simulacrum.{typeclass, op}
+
+//@typeclass trait Semigroup[A] {
+//  @op("|+|") def combine(x: A, y: => A): A
+//}
+//
+//object Semigroup {
+//
+//  def instance[A](combine: (A, => A) => A): Semigroup[A] = {
+//    val combine0 = combine
+//    new Semigroup[A] {
+//      def combine(x: A, y : A) = combine0(x, y)
+//    }
+//  }
+//}
