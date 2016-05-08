2ddcab8575f987a6c0d58263c59f54dc758794a2
lemastero <adres.mistrza@gmail.com>
2016-05-08 17:40:47 +0200

    add comments based of information from Typeclassopedia; Functors implementation for Future, Either, Tuple1, TODOs with articles, and more ideas for functors, usage of kind-projector

--- src/main/scala/Functor.scala
+++ src/main/scala/Functor.scala
@@ -1,24 +1,66 @@
 import simulacrum._
+import scala.concurrent.Future
 import scala.language.higherKinds
 
 /*
- * Based on awesome video serie:
- * Functional Structures in Scala by Michael Pilquist
- * FSiS Part 1 - Type Constructors, Functors, and Kind Projector
- *   https://www.youtube.com/watch?v=Dsd4pc99FSY
- * FSiS Part 2 - Applicative type class
- *   https://youtu.be/tD_EyIKqqCk
+ * Implementation is based on:
+ * Michael Pilquist, Functional Structures in Scala, Part (1) and (2)
  */
 
-/** Covariant functor */
+/**
+  * Covariant functor
+  *
+  * Abstracts over type constructor (generic type like List, Option
+  * not regular type like Integer).
+  *
+  * Interpretation as container (3):
+  *
+  * Functor as container
+  * ====================
+  * Functor abstract over (represents):
+  *  - container (Container[_]) and
+  *  - ability (map) to apply function (f) to every element of that container
+  *
+  * For example:
+  *  - list, tree, map - collection of elements and apply function to every element (List.map)
+  *  - set is not a Functor (map with function that returns const value)
+  *    TODO make instance of a generalization http://article.gmane.org/gmane.comp.lang.haskell.cafe/78052/
+  *    TODO add associative constraint http://blog.omega-prime.co.uk/?p=127
+  *  - option - container that can contain single value or may be empty and ability
+  *     to apply function to its element or ignore function if it does not contain any function
+  *
+  * Functor as computational context
+  * ================================
+  * Functor represents (abstracts over) computational context (Container) and ability to
+  *  apply function to a value (of type A) without changing the context
+  *
+  * For example:
+  *   list - nondeterministic choice: single element can have many possible values (elements of the list)
+  *   option - possible failure
+  *
+  * =====
+  * (1) FSiS Part 1 - Type Constructors, Functors, and Kind Projector https://www.youtube.com/watch?v=Dsd4pc99FSY
+  * (2) FSiS Part 2 - Applicative type class https://www.youtube.com/watch?v=tD_EyIKqqCk
+  * (3) https://wiki.haskell.org/Typeclassopedia#Functor
+  * */
 @typeclass
 trait Functor[Container[_]] { self =>
 
-  /** map a function from A to B on Container with As
-    * to produce Container with B's */
-  def map[A, B](contA: Container[A])(funAtoB: A => B): Container[B]
+  /**
+    * Apply function f to every element of container cont
+    * Output is container of results.
+    *
+    * Does not change the structure of container.
+    *
+    * Haskell:
+    *   class Functor f where
+    *     fmap :: (a -> b) -> f a -> f b
+    */
+  def map[A, B](cont: Container[A])(g: A => B): Container[B]
 
+  // ~~@@@^^^^#####################################################
   // derived methods; they work the right way if map is law abiding
+  // ~~@@@^^^^#####################################################
 
   /** lift function operating of A (type of elements of container) and B
     * to function that transforms Container[A] into Container[B]
@@ -29,9 +71,9 @@ trait Functor[Container[_]] { self =>
     * for option: lift[A, B] (f: A => B): Option[A] => Option[B]
     *   change function to work on Options (instead of arguments)
     * */
-  def lift[A, B](f: A => B): Container[A] => Container[B] =
+  def lift[A, B](g: A => B): Container[A] => Container[B] =
     (contA: Container[A])
-      => map(contA)(f)
+      => map(contA)(g)
 
   /** replace content of Container[A] with with value b
     *
@@ -64,14 +106,14 @@ trait Functor[Container[_]] { self =>
     * for List: fproduct[A, B](list: List[A])(f: A => B): List[(A,B)]
     *   return Option with pair (value, f(value))
     */
-  def fproduct[A, B](contA: Container[A])(f: A => B): Container[(A,B)] = {
+  def fproduct[A, B](contA: Container[A])(g: A => B): Container[(A,B)] = {
     def helper(a: A): (A, B) =
-      (a, f(a))
+      (a, g(a))
 
     map(contA)(helper)
   }
 
-  def zipWith[A, B](contA: Container[A])(f: A => B): Container[(A,B)] = fproduct[A, B](contA)(f)
+  def zipWith[A, B](contA: Container[A])(g: A => B): Container[(A,B)] = fproduct[A, B](contA)(g)
 
   /** Allow for composition of Functors (List of Option of ....) */
   def compose[OtherFunctor[_]](implicit OtherFunctor: Functor[OtherFunctor]): // implicit - ensure OtherFun is functor
@@ -79,49 +121,136 @@ trait Functor[Container[_]] { self =>
           // should be Functor[Container[OtherFun[?]]] but scala can't handle
 
     new Functor[Lambda[OtherContainer => Container[OtherFunctor[OtherContainer]]]] {
-      def map[A, B](composedCont: Container[OtherFunctor[A]])(f: A => B): Container[OtherFunctor[B]] =
-        self.map(composedCont)(ga => OtherFunctor.map(ga)(a => f(a)))
+      def map[A, B](composedCont: Container[OtherFunctor[A]])(g: A => B): Container[OtherFunctor[B]] =
+        self.map(composedCont)(ga => OtherFunctor.map(ga)(a => g(a)))
     }
 }
 
 object Functor {
 
-  implicit val listFunctor: Functor[List] = new Functor[List] {
-    override def map[A, B](list: List[A])(fun: A => B): List[B] =
-      list.map(fun)
+  implicit val listFunctor: Functor[List] =
+    new Functor[List] {
+      def map[A, B](list: List[A])(g: A => B): List[B] =
+        list.map(g)
+    }
+
+  implicit val optionFunctor: Functor[Option] =
+    new Functor[Option] {
+      def map[A, B](option: Option[A])(g: A => B): Option[B] =
+        option.map(g)
+    }
+
+  // functions that takes one argument of type Int: Function1[Int, _]
+  // use syntax from kind-projector
+  implicit val oneArgFunctionsFromInt: Functor[Int => ?] =
+    new Functor[Int => ?] {
+      def map[A, B](g: Int => A)(h: A => B): Int => B =
+        g andThen h
+    }
+
+  // functions that take one argument of some type X: Function1[X, _]
+  // use syntax from kind-projector
+  implicit def oneArgFunctionsFromX[X]: Functor[X => ?] =
+    new Functor[X => ?] {
+      def map[A, B](g: X => A)(h: A => B): X => B =
+        g andThen h
   }
 
-  implicit val optionFunctor: Functor[Option] = new Functor[Option] {
-    override def map[A, B](option: Option[A])(fun: (A) => B): Option[B] =
-      option.map(fun)
-  }
+  type MyTuple1[T] = Tuple2[T, T] // TODO use kind-projector syntactic sugar
+  implicit val tupleSameTypeFunctor: Functor[MyTuple1] =
+    new Functor[MyTuple1] {
+      def map[A, B](tuple: MyTuple1[A])(g: A => B): MyTuple1[B] =
+        (g(tuple._1), g(tuple._2))
+    }
 
-  //  type OneArgFun[X] = Function1[X, _]
-  implicit def oneArgFunctionResultFunctor[X]: Functor[X => ?] = new Functor[X => ?] {
-    override def map[A, B](oneArgFun: X => A)(funAtoB: A => B): X => B =
-      oneArgFun andThen funAtoB
-  }
+  // TODO Functor for Tuple and generic parameter
+
+  type IntEither[T] = Either[Int, T] // TODO use kind-projector syntactic sugar
+  implicit val eitherFunctor: Functor[IntEither] =
+    new Functor[IntEither] {
+      def map[A, B](either: IntEither[A])(g: A => B): IntEither[B] =
+        either match {
+          case Left(ex) => Left(ex)
+          case Right(value) => Right( g(value) )
+        }
+    }
+
+  // TODO Functor for Either with generic parameter
 
   // TODO stream functor
 
-  // TODO future functor
+  implicit val futureFunctor: Functor[Future] =
+    new Functor[Future] {
+      def map[A, B](future: Future[A])(g: A => B): Future[B] =
+        future.map(g)
+    }
+
+  /* TODO reader monad functor
+
+  ((->) e) (which can be thought of as (e ->); see above),
+  the type of functions which take a value of type e as a parameter, is a Functor.
+
+  As a container, (e -> a) represents a (possibly infinite) set of values of a,
+  indexed by values of e.
+
+  Alternatively, and more usefully,
+  ((->) e) can be thought of as a context in which a value of type e
+  is available to be consulted in a read-only fashion.
+
+  This is also why ((->) e) is sometimes referred to as the reader monad;
+  more on this later.
+  */
+
+  /* TODO IO monad functor
+
+  IO is a Functor; a value of type IO a represents a computation producing a value of type a which may have I/O effects.
+  If m computes the value x while producing some I/O effects,
+  then fmap g m will compute the value g x while producing the same I/O effects.
+   */
+
+  /* TODO how Haskell monad for container with annotation is mapped for Scala?
+
+  ((,) e) represents a container which holds an “annotation” of type e along with the actual value it holds.
+  It might be clearer to write it as (e,), by analogy with an operator section like (1+),
+  but that syntax is not allowed in types
+  (although it is allowed in expressions with the TupleSections extension enabled).
+  However, you can certainly think of it as (e,).
+   */
 }
 
+/**
+  * Rules for map function of Functor
+  *
+  * https://hackage.haskell.org/package/base-4.8.2.0/docs/Prelude.html#t:Functor
+  */
 trait FunctorLaws[Container[_]] {
 
   implicit def testedFunctor: Functor[Container]
 
-  // identity law
-  def identity[A](contA: Container[A]): Boolean =
-    testedFunctor.map(contA)(a => a) == contA
+  /** identity law:
+    *
+    * if you use identity function to map over elemnts in Container
+    * then you get the same container
+    *
+    * Haskell:   fmap id  ==  id
+    */
+  def identity[A](cont: Container[A]): Boolean =
+    testedFunctor.map(cont)(a => a) == cont
 
-  // composition law
+  /** composition law
+    *
+    * For any two functions g and h it does not matter
+    * if you combine them and then map over container or
+    * map over container with g and then map over result container with h
+    *
+    * Haskell:    fmap (f . g)  ==  fmap f . fmap g
+    */
   def composition[A, B, C](
-          contA: Container[A],
-          funFirstAtoB: A => B, funSecondBtoC: B => C): Boolean = {
-    val contMapped = testedFunctor.map(contA)(funFirstAtoB)
-    val composedFirstWithSecond = funFirstAtoB andThen funSecondBtoC
-    testedFunctor.map(contMapped)(funSecondBtoC) == testedFunctor.map(contA)(composedFirstWithSecond)
+        cont: Container[A],
+        g: A => B, h: B => C): Boolean = {
+    val contMapped: Container[B] = testedFunctor.map(cont)(g)
+    def composedGH: A => C = g andThen h
+    testedFunctor.map(contMapped)(h) == testedFunctor.map(cont)(composedGH)
   }
 }
 
